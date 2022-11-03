fda2201b7e34e513af8ec0f811a754693b11ab88
fivan999 <fivan999@yandex.ru>
2022-11-03 19:26:33 +0300

    modified

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from random import randint
+from PyQt5.QtCore import Qt
 from PyQt5.QtGui import QPainter, QColor
-from PyQt5.QtWidgets import QWidget, QApplication
+from PyQt5.QtWidgets import QWidget, QApplication, QTableWidgetItem, QHeaderView
 from PyQt5 import uic
 import sys
 from PyQt5 import QtCore, QtGui, QtWidgets
@@ -15,29 +16,31 @@ class Window(QWidget):
     def __init__(self):
         super().__init__()
         uic.loadUi("main.ui", self)
+        self.setWindowTitle("Кофе")
+        self.setFixedSize(680, 281)
         self.load_coffee_data()
+        self.coffee_table_data.setEditTriggers(QtWidgets.QTableWidget.NoEditTriggers)
 
     def load_coffee_data(self):
         query_result = cursor.execute(f"SELECT "
                                       f"coffees.id, sorts.name, fry.name, types.name, tastes.description,"
                                       f"coffees.price, coffees.volume "
                                       f"FROM coffees "
-                                      f"INNER JOIN sorts ON sorts.id = coffees.sort"
-                                      f"INNER JOIN fry.id = coffees.fry"
-                                      f"INNER JOIN types.id = coffees.type"
-                                      f"INNER JOIN tastes.id = coffees.taste "
+                                      f"INNER JOIN sorts ON sorts.id = coffees.sort "
+                                      f"INNER JOIN fry ON fry.id = coffees.fry "
+                                      f"INNER JOIN types ON types.id = coffees.type "
+                                      f"INNER JOIN tastes ON tastes.id = coffees.taste "
                                       ).fetchall()
 
-        self.film_table_data.setRowCount(len(query_result))
-        self.film_table_data.setColumnCount(len(query_result[0]))
+        self.coffee_table_data.setRowCount(len(query_result))
+        self.coffee_table_data.setColumnCount(len(query_result[0]))
         titles = ["ID", "Сорт", "Обжарка", "Тип", "Вкус", "Цена(рубли)", "Обьем(см3)"]
-        self.film_table_data.setHorizontalHeaderLabels(titles)
+        self.coffee_table_data.setHorizontalHeaderLabels(titles)
 
         for i, elem in enumerate(query_result):
             for j, val in enumerate(elem):
-                self.film_table_data.setItem(i, j, QTableWidgetItem(str(val)))
-                # self.film_table_data.item(i, j).setFlags(QtCore.Qt.ItemIsEnabled)
-        self.film_table_data.horizontalHeader().setSectionResizeMode(QHeaderView.Stretch)
+                self.coffee_table_data.setItem(i, j, QTableWidgetItem(str(val)))
+        self.coffee_table_data.horizontalHeader().setSectionResizeMode(QHeaderView.Stretch)
 
 
 def except_hook(cls, exception, traceback):
