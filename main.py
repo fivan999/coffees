e076034890c58563bbdb52f865a06ef1028a3cd1
fivan999 <fivan999@yandex.ru>
2022-11-03 20:21:40 +0300

    add_coffees

--- main.py
+++ main.py
@@ -12,14 +12,54 @@ connection = sqlite3.connect("coffee.db")
 cursor = connection.cursor()
 
 
-class Window(QWidget):
+class CreateCoffee(QWidget):
+    def __init__(self):
+        super().__init__()
+        uic.loadUi("addEditCoffeeForm.ui", self)
+        self.replace_id = None
+        self.initUI()
+
+    def initUI(self):
+        self.setFixedSize(215, 219)
+        self.save_btn.clicked.connect(self.save)
+        sorts = map(lambda x: x[0], cursor.execute("SELECT name FROM sorts").fetchall())
+        fries = map(lambda x: x[0], cursor.execute("SELECT name FROM fry").fetchall())
+        types = map(lambda x: x[0], cursor.execute("SELECT name FROM types").fetchall())
+        tastes = map(lambda x: x[0], cursor.execute("SELECT description FROM tastes").fetchall())
+        self.sort_box.addItems(sorts)
+        self.fry_box.addItems(fries)
+        self.type_box.addItems(types)
+        self.taste_box.addItems(tastes)
+
+    def save(self):
+        sort = self.sort_box.currentIndex() + 1
+        fry = self.fry_box.currentIndex() + 1
+        type_name = self.type_box.currentIndex() + 1
+        taste = self.taste_box.currentIndex() + 1
+        price = self.price_edit.value()
+        volume = self.volume_edit.value()
+        if price and volume:
+            if self.replace_id:
+                cursor.execute("DELETE FROM coffees WHERE id=?", (self.replace_id, ))
+            cursor.execute("INSERT INTO coffees(sort, fry, type, taste, price, volume) "
+                           "VALUES(?, ?, ?, ?, ?, ?)", (sort, fry, type_name, taste,
+                                                        price, volume))
+            connection.commit()
+            window.load_coffee_data()
+            self.close()
+
+
+class AllCoffeesView(QWidget):
     def __init__(self):
         super().__init__()
         uic.loadUi("main.ui", self)
         self.setWindowTitle("Кофе")
-        self.setFixedSize(680, 281)
-        self.load_coffee_data()
+        self.coffee_creation_form = CreateCoffee()
+        self.setFixedSize(680, 331)
+        self.add_coffee_btn.clicked.connect(self.add_coffee)
+        self.edit_coffee_btn.clicked.connect(self.edit_coffee)
         self.coffee_table_data.setEditTriggers(QtWidgets.QTableWidget.NoEditTriggers)
+        self.load_coffee_data()
 
     def load_coffee_data(self):
         query_result = cursor.execute(f"SELECT "
@@ -31,7 +71,7 @@ class Window(QWidget):
                                       f"INNER JOIN types ON types.id = coffees.type "
                                       f"INNER JOIN tastes ON tastes.id = coffees.taste "
                                       ).fetchall()
-
+        print(query_result)
         self.coffee_table_data.setRowCount(len(query_result))
         self.coffee_table_data.setColumnCount(len(query_result[0]))
         titles = ["ID", "Сорт", "Обжарка", "Тип", "Вкус", "Цена(рубли)", "Обьем(см3)"]
@@ -42,6 +82,19 @@ class Window(QWidget):
                 self.coffee_table_data.setItem(i, j, QTableWidgetItem(str(val)))
         self.coffee_table_data.horizontalHeader().setSectionResizeMode(QHeaderView.Stretch)
 
+    def add_coffee(self):
+        self.coffee_creation_form.setWindowTitle("Добавление кофе")
+        self.coffee_creation_form.replace_id = None
+        self.coffee_creation_form.show()
+
+    def edit_coffee(self):
+        row = self.coffee_table_data.currentRow()
+        if row == -1:
+            return
+        self.coffee_creation_form.setWindowTitle("Изменение кофе")
+        self.coffee_creation_form.replace_id = int(self.coffee_table_data.item(row, 0).text())
+        self.coffee_creation_form.show()
+
 
 def except_hook(cls, exception, traceback):
     sys.__excepthook__(cls, exception, traceback)
@@ -50,6 +103,6 @@ def except_hook(cls, exception, traceback):
 if __name__ == "__main__":
     app = QApplication(sys.argv)
     sys.excepthook = except_hook
-    window = Window()
+    window = AllCoffeesView()
     window.show()
     sys.exit(app.exec())
